f2ff3d373331732f19aca4c0d3da0b8c593756c6
amandsha <amandsha@cisco.com>
2018-04-16 12:04:25 +0530

    Initial Commit - 2

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ import {
 } from 'react-native';
 
 import Login from "./app/components/Login";
-import Home from "./app/components/Home";
 import Boiler from "./app/components/Boiler";
 import ForgetPassword from "./app/components/ForgetPassword";
 import Register from "./app/components/Register";
@@ -28,7 +27,23 @@ import { StackNavigator } from "react-navigation";
     'Shake or press menu button for dev menu',
 });*/
 
-
+class Home extends Component<{}> {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "#16a085",
+      elevation: null
+    },
+    header: null
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#16a085" />
+        <Login navigation={this.props.navigation} />
+      </View>
+    );
+  }
+}
 
 export default App = StackNavigator({
   Home: {
