100f6bba6611f79df08209a712397c87e1bf7d57
amandsha <amandsha@cisco.com>
2018-04-16 11:47:24 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+
+import Login from "./app/components/Login";
+import Home from "./app/components/Home";
+import Boiler from "./app/components/Boiler";
+import ForgetPassword from "./app/components/ForgetPassword";
+import Register from "./app/components/Register";
+
+import { StackNavigator } from "react-navigation";
+
+/*const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});*/
+
+
+
+export default App = StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "Home"
+    }
+  },
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      title: "Login"
+    }
+  },
+  Register: {
+    screen: Register,
+    navigationOptions: {
+      title: "Register"
+    }
+  },
+  ForgetPassword: {
+    screen: ForgetPassword,
+    navigationOptions: {
+      title: "ForgetPassword"
+    }
+  },
+  Boiler: {
+    screen: Boiler,
+    navigationOptions: {
+      title: "Boiler"
+    }
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
